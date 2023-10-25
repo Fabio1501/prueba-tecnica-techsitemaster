df5ce39b2d424218d92c5f8333e9a9ebeb6eedd1
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-25 16:04:44 -0300

    styles and routes in front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,32 @@
+import { Route, Routes } from 'react-router-dom'
+import Landing from './components/Landing'
+import NavBar from './components/NavBar'
+import FormCreateClient from './components/FormCreateClient'
+import ViewClients from './components/ViewClients'
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route
+          exact
+          path='/'
+          element={<Landing />}
+        />
+        <Route
+          exact
+          path='/create'
+          element={<FormCreateClient />}
+        />
+        <Route
+          exact
+          path='/view'
+          element={<ViewClients />}
+        />
+      </Routes>
+    </div>
   )
 }
 
