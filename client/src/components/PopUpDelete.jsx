9b34a501f1009a4bd63f4fff7ff104b0c2670350
fabiuuu8@gmail.com <fabiuuu8@gmail.com>
2023-10-25 22:00:41 -0300

    create all flow

--- client/src/components/PopUpDelete.jsx
+++ client/src/components/PopUpDelete.jsx
@@ -1,14 +1,33 @@
-const PopUpDelete = ({ hiddenListOfClients, setHiddenListOfClients }) => {
-    return(
-        <div className={`${hiddenListOfClients ? 'block' : 'hidden'} fixed w-full h-screen flex items-center justify-center top-0 left-0 z-40 md:inset-0 bg-black/70`}>
-            <div className='h-3/4 relative w-1/2 bg-slate-200 text-black rounded-lg p-10'>
-                <button onClick={() => setHiddenListOfClients(!hiddenListOfClients)} className='absolute top-4 right-4'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-                    <path d="M11.8031 10.6031L20.9531 1.45312C21.2906 1.11562 21.2906 0.590625 20.9531 0.253125C20.6156 -0.084375 20.0906 -0.084375 19.7531 0.253125L10.6031 9.40312L1.45312 0.253125C1.11562 -0.084375 0.590625 -0.084375 0.253125 0.253125C-0.084375 0.590625 -0.084375 1.11562 0.253125 1.45312L9.40312 10.6031L0.253125 19.7531C-0.084375 20.0906 -0.084375 20.6156 0.253125 20.9531C0.403125 21.1031 0.628125 21.2156 0.853125 21.2156C1.07812 21.2156 1.30312 21.1406 1.45312 20.9531L10.6031 11.8031L19.7531 20.9531C19.9031 21.1031 20.1281 21.2156 20.3531 21.2156C20.5781 21.2156 20.8031 21.1406 20.9531 20.9531C21.2906 20.6156 21.2906 20.0906 20.9531 19.7531L11.8031 10.6031Z" fill="black" />
-                </svg></button>
-                
-                safasfsfd
-            </div>
+import axios from "axios";
+
+const PopUpDelete = ({ hiddenListOfClients, setHiddenListOfClients, client }) => {
+
+  const deleteClient = async () => {
+    try {
+      const { data } = axios.delete(`http://localhost:3001/user/${client.id}`)
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className={`${hiddenListOfClients ? 'block' : 'hidden'} fixed w-full h-screen flex items-center justify-center top-0 left-0 z-40 md:inset-0 bg-black/70`}>
+      <div className='h-1/2 relative w-1/3 bg-slate-200 text-black rounded-lg p-10'>
+        
+        <button onClick={() => setHiddenListOfClients(!hiddenListOfClients)} className='absolute top-4 right-4'><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+          <path d="M11.8031 10.6031L20.9531 1.45312C21.2906 1.11562 21.2906 0.590625 20.9531 0.253125C20.6156 -0.084375 20.0906 -0.084375 19.7531 0.253125L10.6031 9.40312L1.45312 0.253125C1.11562 -0.084375 0.590625 -0.084375 0.253125 0.253125C-0.084375 0.590625 -0.084375 1.11562 0.253125 1.45312L9.40312 10.6031L0.253125 19.7531C-0.084375 20.0906 -0.084375 20.6156 0.253125 20.9531C0.403125 21.1031 0.628125 21.2156 0.853125 21.2156C1.07812 21.2156 1.30312 21.1406 1.45312 20.9531L10.6031 11.8031L19.7531 20.9531C19.9031 21.1031 20.1281 21.2156 20.3531 21.2156C20.5781 21.2156 20.8031 21.1406 20.9531 20.9531C21.2906 20.6156 21.2906 20.0906 20.9531 19.7531L11.8031 10.6031Z" fill="black" />
+        </svg></button>
+        <div className="h-full flex flex-col gap-y-12 items-center justify-center">
+          <h3 className="text-xl text-center font-semibold ">¿Estás seguro que deseas eliminar a <span className="font-black">{client.name} {client.lastName}</span>?</h3>
+          <p className="text-xl text-center">Si eliminas el cliente no podrás recuperarlo luego.</p>
+          <div className="flex items-center gap-x-4">
+            <button onClick={deleteClient} className="rounded-lg px-4 py-2 font-semibold bg-red-500 text-white">Eliminar</button>
+            <button onClick={() => setHiddenListOfClients(!hiddenListOfClients)} className="rounded-lg px-4 py-2 font-semibold bg-black text-white">Cancelar</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 export default PopUpDelete;
--- client/src/components/ViewClients.jsx
+++ client/src/components/ViewClients.jsx
@@ -1,40 +1,104 @@
 import { useEffect, useState } from "react";
 import axios from 'axios'
-import CardClient from "./CardClient";
+import CardClient, { isoToYMD } from "./CardClient";
 import PopUpDelete from "./PopUpDelete";
+import Charts from "./Charts";
+
+const dataChart = [
+  { name: 'Menores de 30 años', value: 0 },
+  { name: 'De 31 a 60 años', value: 0 },
+  { name: 'Mayores de 60 años', value: 0 }
+];
 
 const ViewClients = () => {
   const [clients, setClients] = useState([])
+  const [data, setData] = useState(dataChart)
   const [hiddenListOfClients, setHiddenListOfClients] = useState(false)
+  const [client, setClient] = useState({
+    id: '',
+    name: '',
+    lastName: ''
+  })
+
+  const [flag, setFlag] = useState(false)
+
 
   const getClients = async () => {
     try {
       const { data } = await axios("http://localhost:3001/user")
       setClients(data)
+      setFlag(true)
     } catch (error) {
       console.log(error);
     }
   }
 
+  function countAgeGroups() {
+    const ages = clients.map(person => {
+      const date = isoToYMD(person.dateOfBirth)
+      const birthDate = new Date(date);
+      const ageInMilliseconds = Date.now() - birthDate.getTime();
+      const age = new Date(ageInMilliseconds).getFullYear() - 1970;
+      return age;
+    });
+
+    const counts = ages.reduce((acc, age) => {
+      if (age < 30) {
+        acc.under30 += 1;
+      } else if (age >= 31 && age <= 60) {
+        acc.between31and60 += 1;
+      } else {
+        acc.over60 += 1;
+      }
+      return acc;
+    }, { under30: 0, between31and60: 0, over60: 0 });
+
+    const newChartData = data.map(dat => {
+      if(dat.name == 'Menores de 30 años'){
+        dat.value = counts.under30
+      }
+      if(dat.name == 'De 31 a 60 años'){
+        dat.value = counts.between31and60
+      }
+      if(dat.name == 'Mayores de 60 años'){
+        dat.value = counts.over60
+      }
+      return dat
+    })
+
+    setData(newChartData)
+  }
+
   useEffect(() => {
-   getClients()
+    getClients()
   }, [])
 
-  return (
-    <div className="w-full flex items-center px-16 gap-x-6">
-      <ul role="list" className=" w-2/3 divide-y divide-gray-400 h-full overflow-y-auto scroll-hidden py-4">
-        {
-          clients?.map((client) => {
-            return (
-              <CardClient setHiddenListOfClients={setHiddenListOfClients} hiddenListOfClients={hiddenListOfClients} name={client.name} lastName={client.lastName} email={client.email} celPhone={client.celPhone} dateOfBirth={client.dateOfBirth} key={client.id} id={client.id} />
-            )
-          })
-        }
-      </ul>
-      <div className="w-1/3 h-screen bg-black">
+  useEffect(() => {
+    if(flag){
+      countAgeGroups()
+    }
+  }, [clients])
 
+  return (
+    <div className="w-full flex  px-16 gap-x-6">
+      <div className=" w-2/3 flex flex-col gap-y-12 h-full py-12">
+        <h3 className="text-3xl font-black ">Lista de clientes</h3>
+        <ul className="overflow-y-auto scroll-hidden divide-y divide-gray-400">
+          {
+            clients.length ?
+              clients.map((client) => {
+                return (
+                  <CardClient setClient={setClient} setHiddenListOfClients={setHiddenListOfClients} hiddenListOfClients={hiddenListOfClients} name={client.name} lastName={client.lastName} email={client.email} celPhone={client.celPhone} dateOfBirth={client.dateOfBirth} key={client.id} id={client.id} />
+                )
+              }) :
+              <h1>Loading...</h1>
+          }
+        </ul>
       </div>
-      <PopUpDelete hiddenListOfClients={hiddenListOfClients} setHiddenListOfClients={setHiddenListOfClients}/>
+      <div className="w-1/3 relative">
+        <Charts data={data} />
+      </div>
+      <PopUpDelete client={client} hiddenListOfClients={hiddenListOfClients} setHiddenListOfClients={setHiddenListOfClients} />
     </div>
   )
 }
